87ffbd695135a7229b3365e48611cc881af927aa
levisliujunqi <liujunqiljqljq@outlook.com>
2024-08-28 20:55:30 +0800

    bbb

---
+++ Untitled-1.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+void solve(){
+    
+}
+signed main(){
+    cin.tie(nullptr)->sync_with_stdio(0);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
